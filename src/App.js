5ba68ad14c2286aea1bb5b94ccd1bce43060fb6f
Volkov <mark.volkovgit@gmail.com>
2021-10-27 00:17:18 +0300

    navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+
 import ContainerForm from "./components/ContainerForm";
 import Login from "./components/form/Login";
 import Header from "./components/Header";
-
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <ContainerForm titles='LOGIN TO YOUR ACCOUNT' >
-        <Login />
-      </ContainerForm>
-    </>
+      <Switch>
+        <Route path='/login'>
+          <ContainerForm titles='LOGIN TO YOUR ACCOUNT' >
+            <Login />
+          </ContainerForm>
+        </Route>
+      </Switch>
+    </BrowserRouter >
   );
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Header.module.sass'
+import { Link } from "react-router-dom"
 export default function Header() {
+    const [swithing, setSwithing] = useState(false)
+
+    const handleBtn = () => {
+        setSwithing(!swithing)
+    }
     return (
         <div className={style.container}>
             <div className={style.containerImg}>
-                <a className={style.headerLink} href="https://formik.org/docs/api/field"><img src='https://www.squadhelp.com/img/logo.png' alt="" /></a>
+                <a className={style.headerLink} href="/"><img src='https://www.squadhelp.com/img/logo.png' alt="" /></a>
             </div>
-            <div className={style.containerBtn}>
-                <button className={style.btn}>Signup</button>
+            <div className={style.containerLink} onClick={handleBtn}>
+                {swithing ? <Link to="/login" className={style.link}>Login</Link> : <Link to="/signup" className={style.link}>SignUp</Link >}
             </div>
         </div>
 
